3202db6fbb38a67d5eab70bbcd4eb8f4941044bc
Tohin666 <tohin666@gmail.com>
2018-08-03 11:51:47 +0300

    lesson4 complete?

--- lesson4/gallery.php
+++ lesson4/gallery.php
@@ -1,5 +1,5 @@
 <?php
-echo '<div class="gallery">';
+echo '<div id="gallery" class="gallery">';
 $images = scandir('./img');
 for ($i = 2; $i < count($images); $i++) {
     echo '<a href="img/' . $images[$i] . '" target = blank><img src="img/' . $images[$i] . '"></a>';
--- lesson4/index.php
+++ lesson4/index.php
@@ -14,5 +14,30 @@ echo '<h1>Lesson4</h1>';
 
 include 'gallery.php';
 ?>
+<form action="" method="post" enctype="multipart/form-data">
+    <input type="file" name="loadedImage">
+    <input type="hidden" name="MAX_FILE_SIZE" value="3000000">
+    <h5>Максимальный размер 3МB</h5>
+    <input type="submit" value="Загрузить фото">
+
+    <?php
+    if (isset($_FILES['loadedImage'])) {
+        $uploadDir = './img/';
+        $uploadFile = $uploadDir . basename($_FILES['loadedImage']['name']);
+
+        echo '<pre>';
+
+        if (move_uploaded_file($_FILES['loadedImage']['tmp_name'], $uploadFile)) {
+            echo '<h3>Фото успешно загружено</h3>';
+
+        } else {
+            echo '<h3>Невозможно загрузить файл!</h3>';
+        }
+
+        echo '</pre>';
+    }
+    ?>
+
+</form>
 </body>
 </html>
