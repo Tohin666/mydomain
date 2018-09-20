024552426701a5f72a8d169c45da6bf9eccbc3ef
Tohin666 <tohin666@gmail.com>
2018-09-20 13:56:02 +0300

    lesson4 complete

--- lesson4/gallery.php
+++ lesson4/gallery.php
@@ -1,7 +1,8 @@
 <?php
 echo '<div id="gallery" class="gallery">';
-$images = scandir(__DIR__ . '/img');
+$images = scandir(__DIR__ . '/img/small');
 for ($i = 2; $i < count($images); $i++) {
-    echo '<a href="img/' . $images[$i] . '" target = blank><img src="img/' . $images[$i] . '"></a>';
+    $bigPictureName = substr_replace($images[$i], '', strrpos($images[$i], '_small'), 6);
+    echo '<a href="img/big/' . $bigPictureName . '" target = blank><img src="img/small/' . $images[$i] . '"></a>';
 }
 echo '</div>';
--- lesson4/index.php
+++ lesson4/index.php
@@ -1,3 +1,52 @@
+<?php
+include __DIR__ . '/funcImgResize.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_FILES['loadedImage'])) {
+
+        $uploadDir = __DIR__ . '/img/big/';
+        if (!file_exists($uploadDir)) {
+            mkdir($uploadDir);
+        }
+
+        $filename = basename($_FILES['loadedImage']['name']);
+        $fileExtension = strrchr($filename, '.');
+        $filenameWithoutExtension =
+            substr_replace($filename, '', -strlen($fileExtension), strlen($fileExtension));
+        $filenameWithPath = $uploadDir . $filename;
+
+        if (file_exists($filenameWithPath)) {
+            $filenameWithoutExtension .= uniqid();
+            $filename = $filenameWithoutExtension . $fileExtension;
+            $filenameWithPath = $uploadDir . $filename; //так проще чем:
+//                $filenameWithPath = substr_replace($filenameWithPath, uniqid(),
+//                    strrpos($filenameWithPath, $fileExtension), 0);
+        }
+
+        echo '<pre>';
+
+        if (substr($_FILES['loadedImage']['type'], 0, 5) == 'image' &&
+            $_FILES['loadedImage']['size'] <= 3000000 &&
+            move_uploaded_file($_FILES['loadedImage']['tmp_name'], $filenameWithPath)) {
+
+            echo '<h3>Фото успешно загружено</h3>';
+
+            $smallImageDir = __DIR__ . '/img/small/';
+            if (!file_exists($smallImageDir)) {
+                mkdir($smallImageDir);
+            }
+
+            $smallFilenameWithPath = $smallImageDir . $filenameWithoutExtension . '_small' . $fileExtension;
+            img_resize($filenameWithPath, $smallFilenameWithPath, 200, 300);
+
+        } else {
+            echo '<h3>Невозможно загрузить файл!</h3>';
+        }
+
+        echo '</pre>';
+    }
+}
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,51 +60,16 @@
 <body>
 <?php
 echo '<h1>Lesson4</h1>';
-include __DIR__ . '/funcImgResize.php';
+
 include __DIR__ . '/gallery.php';
 ?>
 <form action="" method="post" enctype="multipart/form-data">
     <input type="file" name="loadedImage">
-<!--    <input type="hidden" name="MAX_FILE_SIZE" value="3000000">-->
+    <!--    <input type="hidden" name="MAX_FILE_SIZE" value="3000000">-->
     <h5>Максимальный размер 3МB</h5>
     <input type="submit" value="Загрузить фото">
 
-    <?php
-    if ($_SERVER['REQUEST_METHOD'== 'POST']) {
-        echo '<br>';
-        var_dump($_SERVER);
-        echo '<br>';
-        if (isset($_FILES['loadedImage'])) {
-            echo '<br>';
-            var_dump($_FILES['loadedImage']);
-            echo '<br>';
-            $uploadDir = __DIR__ . '/img/big/';
-            if (!file_exists($uploadDir)) {
-                mkdir($uploadDir);
-            }
 
-            $uploadFile = $uploadDir . basename($_FILES['loadedImage']['name']);
-            if (file_exists($uploadFile)) {
-                $uploadFile = uniqid() . $uploadFile;
-            }
-
-            echo '<pre>';
-
-            if (substr($_FILES['loadedImage']['type'], 0, 5) == 'image' &&
-                $_FILES['loadedImage']['size'] <= 3000000 &&
-                move_uploaded_file($_FILES['loadedImage']['tmp_name'], $uploadFile)) {
-                echo '<h3>Фото успешно загружено</h3>';
-                img_resize($uploadFile, __DIR__ . '/img/small/' . , )
-
-        } else {
-                echo '<h3>Невозможно загрузить файл!</h3>';
-            }
-
-            echo '</pre>';
-        }
-    }
-
-    ?>
 
 </form>
 </body>
