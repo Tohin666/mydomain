3fb676a24f6c453c942e657022ec60559364a98d
Tohin666 <tohin666@gmail.com>
2018-09-25 09:04:09 +0300

    lesson4 remake

--- lesson4/templates/galleryTemplate.php
+++ lesson4/templates/galleryTemplate.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lesson4</title>
-    <link rel="stylesheet" href="<?= PUBLIC_DIR ?>style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1>Lesson4</h1>
